440b02053c79218a4785233d43f99e2ff1cdff9f
Nosuriba@11 <kitagawa_taiko@icloud.com>
2020-06-25 17:03:02 +0900

    プレイヤーを左スティック操作で移動できるようにした。
    現在、右スティックの入力を取得できるようにする作業を行いつつ、カメラの回転と移動ができるように
    実装を行っている。

---
+++ ShootingProject/Assets/Scripts/Player/CameraController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Camera camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// 仮の移動処理
+    private void Movement()
+    {
+        if (Input.GetAxis("Vertical2") < 0 &&
+            camera.transform.localEulerAngles.x >= -10)
+        {
+            camera.transform.Rotate(new Vector3(-0.2f, 0, 0));
+        }
+        else if (Input.GetAxis("Vertical2") > 0 &&
+                 camera.transform.localEulerAngles.x <= 10)
+        {
+            camera.transform.Rotate(new Vector3(0.2f, 0, 0));
+        }
+        else {}
+
+        if (Input.GetAxis("Horizontal2") < 0)
+        {
+            camera.transform.Rotate(new Vector3(0, 0.2f, 0));
+        }
+        else if (Input.GetAxis("Horizontal2") > 0)
+        {
+            this.transform.position += new Vector3(0, 0.2f, 0);
+        }
+        else {}
+    }
+}
---
+++ ShootingProject/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    /// 仮の移動処理
+    private void Movement()
+    {
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            this.transform.position += new Vector3(0, 0, -1);
+        }
+        else if (Input.GetAxis("Vertical") > 0)
+        {
+            this.transform.position += new Vector3(0, 0, 1);
+        }
+        else { }
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            this.transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetAxis("Horizontal") > 0)
+        {
+            this.transform.position += new Vector3(1, 0, 0);
+        }
+        else { }
+    }
+
+    private void Jumping()
+    {
+
+    }
+}
