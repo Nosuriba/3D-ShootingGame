3c7dc11ef68bed579d2228dd8319c3615707b59a
Nosuriba@11 <kitagawa_taiko@icloud.com>
2020-06-26 16:15:42 +0900

    カメラを動かした。

--- ShootingProject/Assets/Scripts/Player/CameraController.cs
+++ ShootingProject/Assets/Scripts/Player/CameraController.cs
@@ -4,37 +4,42 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    private Camera camera;      // カメラの情報
+
     [SerializeField]
-    private Camera camera;
+    private GameObject   target;     // カメラのターゲット
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        camera = this.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Movement();
     }
 
     /// 仮の移動処理
     private void Movement()
     {
-        if (Input.GetAxis("Vertical2") < 0 &&
-            camera.transform.localEulerAngles.x >= -10)
+        if (Input.GetAxis("Vertical2") < 0 )
         {
-            camera.transform.Rotate(new Vector3(-0.2f, 0, 0));
+            camera.transform.RotateAround(target.transform.position, this.transform.right, 100 * Time.deltaTime);
         }
-        else if (Input.GetAxis("Vertical2") > 0 &&
-                 camera.transform.localEulerAngles.x <= 10)
+        else if (Input.GetAxis("Vertical2") > 0)
         {
-            camera.transform.Rotate(new Vector3(0.2f, 0, 0));
+            camera.transform.RotateAround(target.transform.position, this.transform.right, -100 * Time.deltaTime);
         }
         else {}
 
         if (Input.GetAxis("Horizontal2") < 0)
         {
-            camera.transform.Rotate(new Vector3(0, 0.2f, 0));
+            camera.transform.RotateAround(target.transform.position, Vector3.up, 100 * Time.deltaTime);
         }
         else if (Input.GetAxis("Horizontal2") > 0)
         {
-            this.transform.position += new Vector3(0, 0.2f, 0);
+            camera.transform.RotateAround(target.transform.position, Vector3.up, -100 * Time.deltaTime);
         }
         else {}
     }
