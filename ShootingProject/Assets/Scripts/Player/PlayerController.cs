243b657e839e8907bc3ed5a112f1c64bd4b466f7
nosuriba <tekito>
2020-07-02 15:18:57 +0900

    とりあえず更新

--- ShootingProject/Assets/Scripts/Player/PlayerController.cs
+++ ShootingProject/Assets/Scripts/Player/PlayerController.cs
@@ -4,22 +4,40 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private Vector3 _vel;           // 速度
+    private Vector3 _vel;                       // 速度
+
+    private Rigidbody rb;                       // RigidBodyの情報取得用  
+
+    [SerializeField]
+    private const float _jumpSpeed = 1.0f;      // ジャンプ時の速度
+
+    private bool _isJump;                       // true : ジャンプ可能, false : ジャンプ不可能
+
+    private const float _maxFallSpeed = -1.0f;  // 落下時の最大速度
     void Start()
     {
-        _vel = new Vector3(0, 0, 0);
+        _vel    = new Vector3(0, 0, 0);
+        rb      = GetComponent<Rigidbody>();
+        _isJump = false;
     }
 
     void Update()
     {
         Movement();
-        Jumping();
+        if (Input.GetKeyDown(KeyCode.Space) && _isJump)
+        {
+            Jumping();
+        }
 
+        if (!_isJump)
+        {
+            Falling();
+        }
         /// 速度の更新
         this.transform.position += _vel;
     }
 
-    /// 仮の移動処理
+    /// 移動処理
     private void Movement()
     {
         /// 速度の初期化
@@ -45,11 +63,28 @@ public class PlayerController : MonoBehaviour
         else { }
     }
 
+    // ジャンプ処理
     private void Jumping()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        _isJump = false;
+        _vel.y  = _jumpSpeed;
+    }
+
+    private void Falling()
+    {
+        /// 落下速度の調整
+        _vel.y = (_vel.y >= _maxFallSpeed ? _vel.y - 0.05f : _vel.y);
+    }
+
+    // 当たり判定が行われている間に入る
+   
+    private void OnCollisionEnter(Collision col)
+    {
+        /// 床に着地した時に入る処理
+        if (col.gameObject.CompareTag("Floor"))
         {
-            _vel.y = 6.0f;
+            _isJump = true;
         }
     }
+
 }
