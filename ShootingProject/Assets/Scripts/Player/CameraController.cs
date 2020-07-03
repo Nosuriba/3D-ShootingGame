d1778b1dae55f381a0ee3e4c274b01f274b62175
nosuriba <tekito>
2020-07-03 14:55:11 +0900

    カメラの回転を行った、

--- ShootingProject/Assets/Scripts/Player/CameraController.cs
+++ ShootingProject/Assets/Scripts/Player/CameraController.cs
@@ -12,25 +12,52 @@ public class CameraController : MonoBehaviour
     [SerializeField]
     private float speed = 2.0f;            // 回転速度
 
+    [SerializeField]
+    private float vAngle= 70;              // 縦角度の限界値 
+
+    private float preX;                 
+    private float nowX;
+
     //呼び出し時に実行される関数
     void Start()
     {
         camera = GetComponent<Camera>();
+        preX = nowX = 0f;
     }
 
     void Update()
     {
+        preX = nowX;
+        nowX = Input.GetAxis("Mouse X");
+
         Rotation();
     }
 
     // カメラの回転を行う
     private void Rotation()
     {
-        //Vector3でX,Y方向の回転の度合いを定義
-        Vector3 angle = new Vector3(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"), 0);
+        // 回転の度合いを設定する
+        Vector3 rotaRate = new Vector3((nowX - preX) * speed, -(Input.GetAxis("Mouse Y") * speed), 0);
 
         /// カメラの回転
-        camera.transform.RotateAround(player.transform.position, Vector3.up, angle.x);
-        camera.transform.RotateAround(player.transform.position, transform.right, angle.y);
+        
+        /// Y軸回転
+        camera.transform.RotateAround(player.transform.position, Vector3.up, rotaRate.x);
+
+        /// X軸回転
+        camera.transform.RotateAround(player.transform.position, transform.right, rotaRate.y);
+
+        //if (camera.transform.localEulerAngles.x >= vAngle)
+        //{
+        //    camera.transform.localEulerAngles = new Vector3(vAngle,
+        //                                                    camera.transform.localEulerAngles.y,
+        //                                                    camera.transform.localEulerAngles.z);
+        //}
+        //else if (camera.transform.localEulerAngles.x <= -vAngle)
+        //{
+        //    camera.transform.localEulerAngles = new Vector3(-vAngle,
+        //                                                    camera.transform.localEulerAngles.y,
+        //                                                    camera.transform.localEulerAngles.z);
+        //}
     }
 }
