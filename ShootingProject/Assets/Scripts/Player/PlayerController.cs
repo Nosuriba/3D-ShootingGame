61aad21f424546c37d90d6e1cf20fba6a163b654
nosuriba <tekito>
2020-07-15 13:06:23 +0900

    ジャンプ処理と移動処理の修正が終わった

--- ShootingProject/Assets/Scripts/Player/PlayerController.cs
+++ ShootingProject/Assets/Scripts/Player/PlayerController.cs
@@ -7,20 +7,24 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private Camera _camera;
 
+    [SerializeField]
+    private float _gravity = 0.2f;
+
     private Rigidbody _rb;
 
-    private Vector3 _vel;
+    private float _fallSpeed;                   // ジャンプ中の速度(名前を変えたほうがいいかも)
 
-    private const float _jumpSpeed = 1.0f;      // ジャンプ時の速度
+    private const float _jumpSpeed = 3.0f;      // ジャンプ時の速度
 
     private bool _isJump;                       // true : ジャンプ可能, false : ジャンプ不可能
 
-    private const float _maxFallSpeed = -1.0f;  // 落下時の最大速度
+    private const float _maxFallSpeed = -3.0f;  // 落下時の最大速度
     void Start()
     {
         _camera     = GameObject.FindWithTag("MainCamera").GetComponent<Camera>();
         _rb         = GetComponent<Rigidbody>();
         _isJump     = false;
+        _fallSpeed  = 0;
     }
 
     private void Update()
@@ -33,52 +37,45 @@ public class PlayerController : MonoBehaviour
 
         if (!_isJump)
         {
-          Falling();
+          　Falling();
         }
-
-        /// ジャンプ処理
-          //this.transform.position += Vector3.up * _speed;
-        // _rigidBody.MovePosition(this.transform.position + (Vector3.up * _speed) * Time.deltaTime);
-
         /// カメラの正面方向に角度を合わせる
         this.transform.localEulerAngles = new Vector3(0, _camera.transform.eulerAngles.y, 0);
-
-        _rb.velocity *= 0.85f;
     }
 
     private void FixedUpdate()
     {
-        /// RigidBodyに力を加える(速度を加算させている)
-        _rb.AddForce(_vel, ForceMode.VelocityChange);
-
-        Vector3 gravity = new Vector3(0, _vel.y, 0);
-        _rb.AddForce(gravity, ForceMode.Acceleration);
-        /// 速度制限
-        Debug.Log(_rb.velocity);
+        Movement();
+       
     }
 
     /// 移動処理
     private void Movement()
     {
-        _vel = Vector3.zero;
+        /// 移動値の取得
+        Vector3 vel = new Vector3(Input.GetAxisRaw("Horizontal") * 10,
+                                  _fallSpeed,
+                                  Input.GetAxisRaw("Vertical") * 10);
 
+        /// RigidBodyに力を加える(速度を加算させている)
+        _rb.AddForce(vel, ForceMode.VelocityChange);
+        /// 速度制限
+        Debug.Log(_rb.velocity);
 
-        _vel = new Vector3(Input.GetAxisRaw("Horizontal") * 10, 0, Input.GetAxisRaw("Vertical") * 10);
+        /// 速度の減速
+        _rb.velocity *= 0.85f;
     }
 
     // ジャンプ処理
     private void Jumping()
     {
-        _isJump = false;
-        _vel.y  = _jumpSpeed;
+        _isJump    = false;
+        _fallSpeed = _jumpSpeed;
     }
 
     private void Falling()
     {
-        /// 落下速度の調整
-        // _vel.y = (_vel.y >= _maxFallSpeed ? _vel.y - 0.02f : _vel.y);
-
-        _vel.y -= 2f;
+        _fallSpeed = (_fallSpeed >= _maxFallSpeed ? _fallSpeed - _gravity : _fallSpeed);
     }
 
     // 当たり判定が行われている間に入る(敵キャラと当たった時などで使用する)
@@ -88,7 +85,7 @@ public class PlayerController : MonoBehaviour
         if (col.gameObject.CompareTag("Floor"))
         {
             _isJump = true;
-            _vel.y = 0;
+            //_vel.y = 0;
         }
     }
 
@@ -96,7 +93,7 @@ public class PlayerController : MonoBehaviour
     {
         if (_isJump)
         {
-            _vel.y = 0;
+            //_vel.y = 0;
         }
        
         _isJump = false;
